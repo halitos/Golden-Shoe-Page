707452f78690b9640657259dcc571a39826060ae
halit <oskan96@gmail.com>
2021-01-05 14:09:51 +0000

    some form stlye added

--- src/Components/FormArea.js
+++ src/Components/FormArea.js
@@ -2,16 +2,16 @@ import React from "react";
 import { Form, FormControl, Button } from "react-bootstrap";
 const FormArea = () => {
   return (
-    <Form className="ml-2 p-3 w-25 bg-light">
+    <Form className="ml-2 p-2 w-25 bg-light">
       <Form inline>
-        <Form.Label>Search for keywords</Form.Label>
+        <Form.Label className="my-3">Search for keywords</Form.Label>
         <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
         <Button variant="info" type="submit">
           Submit
         </Button>
       </Form>
       <Form.Group controlId="exampleForm.ControlSelect1">
-        <Form.Label>Categories</Form.Label>
+        <Form.Label className="my-3">Categories</Form.Label>
         <Form.Control as="select" type="text" placeholder="select">
           <option>Select</option>
           <option>Women</option>
@@ -21,7 +21,7 @@ const FormArea = () => {
         </Form.Control>
       </Form.Group>
       <Form.Group controlId="exampleForm.ControlSelect2">
-        <Form.Label>Example multiple select</Form.Label>
+        <Form.Label>Multiple select</Form.Label>
         <Form.Control as="select" multiple>
           <option></option>
           <option>Women</option>
