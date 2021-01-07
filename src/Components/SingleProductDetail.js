ce092b354f7f7fee23f04791066de8028834b0a0
halit <oskan96@gmail.com>
2021-01-07 21:22:48 +0000

    small imgs and zoom funct added/ improved response

--- src/Components/SingleProductDetail.js
+++ src/Components/SingleProductDetail.js
@@ -1,19 +1,20 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
+import ReactImageMagnify from "react-image-magnify";
 import brown_shoe from "../images/brown_shoe.PNG";
+import shoe_1 from "../images/shoe_1.jpg";
+import shoe_2 from "../images/shoe_2.jpg";
+import shoe_3 from "../images/shoe_3.jpg";
+import shoe_4 from "../images/shoe_4.jpg";
+import shoe_big from "../images/shoe_big.jpg";
+import SizeButtons from "./SizeButtons";
 import SizeGuideModal from "./SizeGuideModal";
-import AmountContext from "./Context";
 
 const SingleProductDetail = () => {
-  const { amount, setAmount } = useContext(AmountContext);
   const [show, setShow] = useState(false);
+  const shoeImgSrcs = [shoe_1, shoe_2, shoe_3, shoe_4];
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  function handleClick() {
-    !amount ? setAmount(true) : setAmount(false);
-    console.log(amount);
-  }
 
   return (
     <>
@@ -22,47 +23,50 @@ const SingleProductDetail = () => {
         handleShow={handleShow}
         show={show}
       />
-      <div className="container d-flex my-5">
-        <img src={brown_shoe} alt="sample shoe" className="w-75 h-50"></img>
-        <div>
-          <h3>Fab Brown Shoe</h3>
-          <p>This is the sigle product details page to be worked on later...</p>
-          <p>Select Size</p>
-          <button className="btn btn-sm text-success" onClick={handleShow}>
-            Size guide
-          </button>
-          <div className="container mt-2">
-            <button className="size-select btn btn-sm border-danger m-3">
-              7.5
-            </button>
-            <button className="size-select btn btn-sm border-danger m-3 ">
-              8
-            </button>
-            <button className="size-select btn btn-sm border-danger m-3">
-              8.5
-            </button>
+      <div className="row my-5 mx-2">
+        <div className="col-lg-7 col-sm-12 mb-3">
+          <ReactImageMagnify
+            {...{
+              smallImage: {
+                alt: "sample shoe",
+                isFluidWidth: true,
+                src: brown_shoe,
+              },
+              largeImage: {
+                src: shoe_big,
+                width: 3000,
+                height: 2000,
+              },
+              isHintEnabled: true,
+              shouldHideHintAfterFirstActivation: false,
+              enlargedImagePosition: "over",
+            }}
+          />
+          <div className="sml-img-box d-flex p-1">
+            <img
+              src={shoeImgSrcs[0]}
+              alt="sample shoe"
+              className="w-25 m"
+            ></img>
+            <img
+              src={shoeImgSrcs[1]}
+              alt="sample shoe"
+              className="w-25 mx-1"
+            ></img>
+            <img
+              src={shoeImgSrcs[2]}
+              alt="sample shoe"
+              className="w-25 mx"
+            ></img>
+            <img
+              src={shoeImgSrcs[3]}
+              alt="sample shoe"
+              className="w-25 mx-1"
+            ></img>
           </div>
-          <div className="container mb-5">
-            <button
-              className={
-                amount
-                  ? "size-select btn btn-sm border-danger m-3"
-                  : "size-select btn btn-sm border-success m-3 "
-              }
-            >
-              9
-            </button>
-            <button className="size-select btn btn-sm border-danger m-3">
-              9.5
-            </button>
-            <button className="size-select btn btn-sm border-danger m-3">
-              10
-            </button>
-          </div>
-
-          <button className="btn btn-dark" onClick={handleClick}>
-            {!amount ? "Add to Basket" : "Remove"}
-          </button>
+        </div>
+        <div className="col-lg-4 col-sm-12">
+          <SizeButtons handleShow={handleShow} />
         </div>
       </div>
     </>
---
+++ src/Components/SizeButtons.js
@@ -0,0 +1,52 @@
+import React, { useContext } from "react";
+import AmountContext from "./Context";
+
+function SizeButtons({ handleShow }) {
+  const { amount, setAmount } = useContext(AmountContext);
+
+  function handleClick() {
+    !amount ? setAmount(true) : setAmount(false);
+    console.log(amount);
+  }
+
+  return (
+    <div className="container single-sizebox">
+      <h3>Fab Brown Shoe</h3>
+      <p className="lead">£125</p>
+      <p>This is the sigle product details page to be worked on later...</p>
+      <span className="lead mr-5">Select Size</span>
+      <button className="btn btn-sm text-success ml-5" onClick={handleShow}>
+        Size guide
+      </button>
+      <div className="container mt-2">
+        <button className="size-select btn btn-sm border-danger m-3">
+          7.5
+        </button>
+        <button className="size-select btn btn-sm border-danger m-3 ">8</button>
+        <button className="size-select btn btn-sm border-danger m-3">
+          8.5
+        </button>
+      </div>
+      <div className="container mb-4">
+        <button
+          className={
+            amount
+              ? "size-select btn btn-sm border-danger m-3"
+              : "size-select btn btn-sm border-success m-3 "
+          }
+        >
+          9
+        </button>
+        <button className="size-select btn btn-sm border-danger m-3">
+          9.5
+        </button>
+        <button className="size-select btn btn-sm border-danger m-3">10</button>
+      </div>
+      <button className="btn btn-secondary add-btn" onClick={handleClick}>
+        {!amount ? "Add to Basket" : "Remove"}
+      </button>
+    </div>
+  );
+}
+
+export default SizeButtons;
