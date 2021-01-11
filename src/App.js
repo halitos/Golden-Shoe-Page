dee731f3040759a0b8ce20b2744df3739be11993
halit <oskan96@gmail.com>
2021-01-11 20:54:41 +0000

    fetch trial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import CarouselBox from "./Components/CarouselBox";
 import Footer from "./Components/Footer";
@@ -12,17 +12,30 @@ import AmountContext from "./Components/Context";
 function App() {
   const [amount, setAmount] = useState(false);
   const value = { amount, setAmount };
+  const [prods, setProds] = useState();
+
+  useEffect(() => {
+    fetch("/products")
+      .then((res) => res.json())
+      .then((res) => {
+        setProds(res);
+      });
+  }, []);
+
+  console.log(prods);
+
   return (
     <>
       <AmountContext.Provider value={value}>
         <NavbarBrand />
+        {prods !== undefined ? <div>{prods.products[0].title}</div> : "nothing"}
         <Switch>
           <Route exact path="/">
             <div className="main row ml-3">
               <FormArea />
               <CarouselBox />
             </div>
-            <ProductCardsDisplay />
+            <ProductCardsDisplay prods={prods} />
           </Route>
           <Route path="/details">
             <SingleProductDetail />
--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
-const products = require("./products");
 const express = require("express");
+const products = require("./products");
 const path = require("path");
 const app = express();
 
-const { Pool } = require("pg");
-app.use(express.json());
+// const { Pool } = require("pg");
+// app.use(express.json());
 
-const db = new Pool({
-  user: "44788",
-  host: "localhost",
-  database: "golden_shoe",
-  password: "*****",
-  port: 5432,
-});
+// const db = new Pool({
+//   user: "44788",
+//   host: "localhost",
+//   database: "golden_shoe",
+//   password: "*****",
+//   port: 5432,
+// });
 
-app.get("/amount", function (req, res) {
-  db.query(`SELECT * FROM products where id=1`)
-    .then((result) => {
-      if (result) res.json(result.rows);
-    })
-    .catch((err) => res.status(500).json({ error: err }));
-});
+// app.get("/amount", function (req, res) {
+//   db.query(`SELECT * FROM products where id=1`)
+//     .then((result) => {
+//       if (result) res.json(result.rows);
+//     })
+//     .catch((err) => res.status(500).json({ error: err }));
+// });
 
 app.use(express.static(path.join(__dirname, "build")));
 
@@ -32,6 +32,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(8080, function () {
-  console.log("Server is listening on port 8080. Ready to accept requests!");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function () {
+  console.log(`Server is listening on port ${PORT}. Ready to accept requests!`);
 });
